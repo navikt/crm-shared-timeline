623961afe4459cda28dcb2f4b1dbe4f032fd3803
John Føreland <john.foreland@soprasteria.com>
2021-04-15 16:16:02 +0200

    renamed component

---
+++ force-app/main/default/lwc/objectTimeline/labels.js
@@ -0,0 +1,47 @@
+import MomentJsLanguage from '@salesforce/label/c.ObjectTimeline_MomentJsLanguage';
+import newObj from '@salesforce/label/c.ObjectTimeline_New';
+import upcoming from '@salesforce/label/c.ObjectTimeline_Upcoming';
+import thisMonth from '@salesforce/label/c.ObjectTimeline_ThisMonth';
+import previousMonth from '@salesforce/label/c.ObjectTimeline_PreviousMonth';
+import overdue from '@salesforce/label/c.ObjectTimeline_Overdue';
+import older from '@salesforce/label/c.ObjectTimeline_Older';
+import emptyGroup from '@salesforce/label/c.ObjectTimeline_EmptyGroup';
+import loadMore from '@salesforce/label/c.ObjectTimeline_LoadMore';
+import viewMore from '@salesforce/label/c.ObjectTimeline_ViewMore';
+import viewAll from '@salesforce/label/c.ObjectTimeline_ViewAll';
+import expand from '@salesforce/label/c.ObjectTimeline_Expand';
+import collapse from '@salesforce/label/c.ObjectTimeline_Collapse';
+import activities from '@salesforce/label/c.ObjectTimeline_Activities';
+import emptyTitle from '@salesforce/label/c.ObjectTimeline_EmptyTitle';
+import emptySubtitle from '@salesforce/label/c.ObjectTimeline_EmptySubtitle';
+import errorTitle from '@salesforce/label/c.ObjectTimeline_ErrorTitle';
+import today from '@salesforce/label/c.ObjectTimeline_Today';
+import tomorrow from '@salesforce/label/c.ObjectTimeline_Tomorrow';
+import yesterday from '@salesforce/label/c.ObjectTimeline_Yesterday';
+import last from '@salesforce/label/c.ObjectTimeline_Last';
+import allDataLoaded from '@salesforce/label/c.ObjectTimeline_AllDataLoaded';
+
+export default {
+    MomentJsLanguage,
+    newObj,
+    upcoming,
+    older,
+    loadMore,
+    viewMore,
+    viewAll,
+    overdue,
+    previousMonth,
+    thisMonth,
+    collapse,
+    expand,
+    activities,
+    emptyTitle,
+    emptySubtitle,
+    errorTitle,
+    emptyGroup,
+    today,
+    tomorrow,
+    yesterday,
+    last,
+    allDataLoaded
+};
