3e9cf70c918f9e10e0a90811f1f628d1e97197d2
John Føreland <john.foreland@soprasteria.com>
2021-04-16 08:53:20 +0200

    renamed again to timeline only

---
+++ force-app/main/default/lwc/timelineGroup/timelineGroup.js
@@ -0,0 +1,44 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class TimelineGroup extends LightningElement {
+    @api group;
+    @api labels;
+    @api amountOfRecords;
+    @api amountOfRecordsToLoad;
+    @api openAccordionSections;
+
+    amount;
+    empty = false;
+
+    connectedCallback() {
+        this.amount = this.amountOfRecords;
+    }
+
+    renderedCallback() {
+        this.empty = this.group.models.length === 0;
+    }
+
+    get showViewMore() {
+        return this.amount < this.group.models.length;
+    }
+
+    viewMore() {
+        this.amount += this.amountOfRecordsToLoad;
+    }
+
+    viewAll() {
+        this.amount = this.group.models.length;
+    }
+
+    get total() {
+        return this.group.models.length;
+    }
+
+    get isOpen() {
+        return this.openAccordionSections.includes(this.group.id);
+    }
+
+    get isClosed() {
+        return !this.isOpen;
+    }
+}
