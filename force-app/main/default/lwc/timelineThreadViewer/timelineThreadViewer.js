db3f4b72604be300a06c5eeacacf6c7d2677a8bc
Eugenijus <eugenijus.margalikas@nav.no>
2022-08-25 08:07:05 +0200

    fix guard for null and undefined

--- force-app/main/default/lwc/timelineThreadViewer/timelineThreadViewer.js
+++ force-app/main/default/lwc/timelineThreadViewer/timelineThreadViewer.js
@@ -4,15 +4,17 @@ import getmessages from '@salesforce/apex/Timeline_ThreadViewController.getMessa
 export default class TimelineThreadViewer extends LightningElement {
     @api threadid;
     @track messages;
+    hasMessages = false;
     error;
 
-    @wire(getmessages, { threadId: '$threadId' }) //Calls apex and extracts messages related to this record
+    @wire(getmessages, { threadId: '$threadid' }) //Calls apex and extracts messages related to this record
     wiremessages(result) {
         if (result.error) {
             this.error = result.error;
             console.log('Error: ' + JSON.stringify(error, null, 2));
         } else if (result.data) {
             this.messages = result.data;
+            hasMessages = true;
             console.log(result.data);
         }
     }
