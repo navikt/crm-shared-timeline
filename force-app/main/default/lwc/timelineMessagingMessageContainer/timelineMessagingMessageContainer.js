051ae7645a48515d419d2d0da0beb90fb12c65e7
Eugenijus <eugenijus.margalikas@nav.no>
2022-08-25 14:51:35 +0200

    cleanup

--- force-app/main/default/lwc/timelineMessagingMessageContainer/timelineMessagingMessageContainer.js
+++ force-app/main/default/lwc/timelineMessagingMessageContainer/timelineMessagingMessageContainer.js
@@ -2,8 +2,6 @@ import { LightningElement, api } from 'lwc';
 import Id from '@salesforce/user/Id';
 
 export default class TimelineMessagingMessageContainer extends LightningElement {
-    showpopover = false;
-    isreply = false;
     @api message;
     userid;
 
@@ -18,21 +16,5 @@ export default class TimelineMessagingMessageContainer extends LightningElement 
 
     get isevent() {
         return this.message.CRM_Type__c === 'Event';
-    }
-
-    //if there is a reply, hide it
-    get showReplyButton() {
-        return typeof this.message.Previous_Message__c !== 'undefined';
-    }
-
-    showdata() {
-        this.showpopover = true;
-    }
-    hidedata() {
-        this.showpopover = false;
-    }
-    replythreadPressed() {
-        const selectedEvent = new CustomEvent('answerpressed', { detail: this.message.Id });
-        this.dispatchEvent(selectedEvent);
     }
 }
