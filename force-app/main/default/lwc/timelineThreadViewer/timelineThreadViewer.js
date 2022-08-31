06e9dae916dadd6d2a080e9c795ffb88e7cd27e6
Eugenijus <eugenijus.margalikas@nav.no>
2022-08-31 12:43:30 +0200

    added journalforing to custom view

--- force-app/main/default/lwc/timelineThreadViewer/timelineThreadViewer.js
+++ force-app/main/default/lwc/timelineThreadViewer/timelineThreadViewer.js
@@ -1,9 +1,13 @@
 import { LightningElement, api, track, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import getmessages from '@salesforce/apex/Timeline_ThreadViewController.getMessagesFromThread';
+
+const FIELDS = [ 'Thread__c.CRM_Journal_Status_Formula__c' ];
 
 export default class TimelineThreadViewer extends LightningElement {
     @api recordId;
     @track messages;
+    journalforing;
     hasMessages = false;
     error;
 
@@ -15,6 +19,17 @@ export default class TimelineThreadViewer extends LightningElement {
         } else if (result.data) {
             this.messages = result.data;
             this.hasMessages = true;
+        }
+    }
+    @wire(getRecord, {
+        recordId: '$recordId',
+        fields: '$FIELDS'
+    })
+    deWireRecord({ data, error }) {
+        if (data) {
+            this.journalforing = getFieldValue(data, FIELDS[0]);
+        } else if (error) {
+            //Something went terribly wrong
         }
     }
 }
