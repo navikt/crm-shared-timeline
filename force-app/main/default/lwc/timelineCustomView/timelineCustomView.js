675b287a27b3c41e64daf5528ad8b0d06585e58e
Morten Mikalsen <morten.mikalsen@hotmail.com>
2025-01-07 10:08:35 +0100

    Send logEvent down to custom component

--- force-app/main/default/lwc/timelineCustomView/timelineCustomView.js
+++ force-app/main/default/lwc/timelineCustomView/timelineCustomView.js
@@ -2,6 +2,7 @@ import { LightningElement, api } from 'lwc';
 export default class TimelineCustomView extends LightningElement {
     @api recordId;
     @api customComponentName;
+    @api logEvent;
 
     customComponent;
 
