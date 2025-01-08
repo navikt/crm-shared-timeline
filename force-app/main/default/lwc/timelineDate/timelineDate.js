4d52c3c4fdd12b3c04aabd36f5ed826a933658ea
Morten Mikalsen <43532815+mamikals@users.noreply.github.com>
2025-01-08 08:43:48 +0100

    Reform getDateFormat to be more readable
    Co-authored-by: eirikfladby <eirik.fladby@gmail.com>

--- force-app/main/default/lwc/timelineDate/timelineDate.js
+++ force-app/main/default/lwc/timelineDate/timelineDate.js
@@ -7,35 +7,39 @@ export default class TimelineDate extends LightningElement {
     @api period;
     @api labels;
 
-    get getDateFormat() {
-        try {
-            if (this.period === this.labels.upcoming || this.period === this.labels.overdue) {
-                var settings = this.isDate
-                    ? {
-                          sameDay: '[' + this.labels.today + ']',
-                          nextDay: '[' + this.labels.tomorrow + ']',
-                          nextWeek: 'dddd',
-                          lastDay: '[' + this.labels.yesterday + ']',
-                          lastWeek: '[' + this.labels.last + '] dddd',
-                          sameElse: 'DD.MM.YYYY'
-                      }
-                    : {
-                          sameDay: '[' + this.labels.today + ' ' + this.labels.timePrefix + '] HH:mm',
-                          nextDay: '[' + this.labels.tomorrow + ' ' + this.labels.timePrefix + '] HH:mm',
-                          nextWeek: 'DD.MM.YYYY [' + this.labels.timePrefix + '] HH:mm',
-                          lastDay: '[' + this.labels.yesterday + ' ' + this.labels.timePrefix + '] HH:mm',
-                          lastWeek: 'DD.MM.YYYY [' + this.labels.timePrefix + '] HH:mm',
-                          sameElse: 'DD.MM.YYYY [' + this.labels.timePrefix + '] HH:mm'
-                      };
-                return moment(this.dateValueDb).calendar(null, settings);
-            } else {
-                return moment(this.dateValueDb)
-                    .format(this.isDate ? 'L' : 'L [' + this.labels.timePrefix + '] HH:mm')
-                    .replaceAll('/', '.');
-            }
-        } catch (error) {
-            console.log(error);
-            return this.dateValueDb;
+get getDateFormat() {
+    try {
+        const baseSettings = {
+            sameDay: `[${this.labels.today}]`,
+            nextDay: `[${this.labels.tomorrow}]`,
+            nextWeek: 'dddd',
+            lastDay: `[${this.labels.yesterday}]`,
+            lastWeek: `[${this.labels.last}] dddd`,
+            sameElse: 'DD.MM.YYYY'
+        };
+
+        const timeSettings = {
+            sameDay: `[${this.labels.today} ${this.labels.timePrefix}] HH:mm`,
+            nextDay: `[${this.labels.tomorrow} ${this.labels.timePrefix}] HH:mm`,
+            nextWeek: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`,
+            lastDay: `[${this.labels.yesterday} ${this.labels.timePrefix}] HH:mm`,
+            lastWeek: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`,
+            sameElse: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`
+        };
+
+        if (this.period === this.labels.upcoming || this.period === this.labels.overdue) {
+            const settings = this.isDate ? baseSettings : timeSettings;
+            return moment(this.dateValueDb).calendar(null, settings);
         }
+
+        const format = this.isDate ? 'L' : `L [${this.labels.timePrefix}] HH:mm`;
+        return moment(this.dateValueDb)
+            .format(format)
+            .replaceAll('/', '.');
+    } catch (error) {
+        console.error('Error formatting date:', error);
+        return null;
+    }
+}
     }
 }
