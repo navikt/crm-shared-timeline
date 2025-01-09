ff7a5fbfe265bfa17d71c945d80f55491c5bf256
Morten Mikalsen <morten.mikalsen@hotmail.com>
2025-01-09 08:26:17 +0100

    Fix unexpected token error

--- force-app/main/default/lwc/timelineDate/timelineDate.js
+++ force-app/main/default/lwc/timelineDate/timelineDate.js
@@ -7,39 +7,36 @@ export default class TimelineDate extends LightningElement {
     @api period;
     @api labels;
 
-get getDateFormat() {
-    try {
-        const baseSettings = {
-            sameDay: `[${this.labels.today}]`,
-            nextDay: `[${this.labels.tomorrow}]`,
-            nextWeek: 'dddd',
-            lastDay: `[${this.labels.yesterday}]`,
-            lastWeek: `[${this.labels.last}] dddd`,
-            sameElse: 'DD.MM.YYYY'
-        };
+    get getDateFormat() {
+        try {
+            const baseSettings = {
+                sameDay: `[${this.labels.today}]`,
+                nextDay: `[${this.labels.tomorrow}]`,
+                nextWeek: 'dddd',
+                lastDay: `[${this.labels.yesterday}]`,
+                lastWeek: `[${this.labels.last}] dddd`,
+                sameElse: 'DD.MM.YYYY'
+            };
 
-        const timeSettings = {
-            sameDay: `[${this.labels.today} ${this.labels.timePrefix}] HH:mm`,
-            nextDay: `[${this.labels.tomorrow} ${this.labels.timePrefix}] HH:mm`,
-            nextWeek: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`,
-            lastDay: `[${this.labels.yesterday} ${this.labels.timePrefix}] HH:mm`,
-            lastWeek: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`,
-            sameElse: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`
-        };
+            const timeSettings = {
+                sameDay: `[${this.labels.today} ${this.labels.timePrefix}] HH:mm`,
+                nextDay: `[${this.labels.tomorrow} ${this.labels.timePrefix}] HH:mm`,
+                nextWeek: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`,
+                lastDay: `[${this.labels.yesterday} ${this.labels.timePrefix}] HH:mm`,
+                lastWeek: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`,
+                sameElse: `DD.MM.YYYY [${this.labels.timePrefix}] HH:mm`
+            };
 
-        if (this.period === this.labels.upcoming || this.period === this.labels.overdue) {
-            const settings = this.isDate ? baseSettings : timeSettings;
-            return moment(this.dateValueDb).calendar(null, settings);
+            if (this.period === this.labels.upcoming || this.period === this.labels.overdue) {
+                const settings = this.isDate ? baseSettings : timeSettings;
+                return moment(this.dateValueDb).calendar(null, settings);
+            }
+
+            const format = this.isDate ? 'L' : `L [${this.labels.timePrefix}] HH:mm`;
+            return moment(this.dateValueDb).format(format).replaceAll('/', '.');
+        } catch (error) {
+            console.error('Error formatting date:', error);
+            return null;
         }
-
-        const format = this.isDate ? 'L' : `L [${this.labels.timePrefix}] HH:mm`;
-        return moment(this.dateValueDb)
-            .format(format)
-            .replaceAll('/', '.');
-    } catch (error) {
-        console.error('Error formatting date:', error);
-        return null;
-    }
-}
     }
 }
